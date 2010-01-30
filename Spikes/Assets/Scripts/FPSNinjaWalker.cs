405c04e07dcdea60e242e8ab4532b85ece3bb013
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-30 15:55:09 +0000

    Enabled "Edit|Project Settings|Editor|Enable external version control".

--- Spikes/Assets/Scripts/FPSNinjaWalker.cs
+++ Spikes/Assets/Scripts/FPSNinjaWalker.cs
@@ -4,26 +4,52 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class FPSNinjaWalker : MonoBehaviour 
 {
-
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+	
+	public bool useMouse = true;
+	
 	public float speed = 6.0f;
-	public float jumpSpeed = 8.0f;
 	public float gravity = 20.0f;
 
 	private Vector3 moveDirection = Vector3.zero;
 	private bool grounded = false;
+	
+	private float lastDropTime = 0.0f;
+	
+	private Vector3 mouseOnPlane;
 
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawSphere(mouseOnPlane, 1.0f);
+	}	
+	
 	void FixedUpdate () 
 	{
 		if (grounded) 
 		{
 			// We are grounded, so recalculate movedirection directly from axes
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-		
-			if (Input.GetButton ("Jump")) 
+			if (useMouse)
 			{
-				moveDirection.y = jumpSpeed;
+				//if (Input.mouseOverWindow)
+				{
+					Camera cam = Camera.main;
+					Vector3 pos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+					Vector3 dir = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane - 1)) - pos;
+					GameObject plane = GameObject.Find("Plane");
+					Vector3 planeNormal = plane.transform.TransformDirection(Vector3.up);
+					float d = -Vector3.Dot(planeNormal, plane.transform.position);
+					
+					float planeDistance = Vector3.Dot(pos, planeNormal) + d;
+					mouseOnPlane = pos + dir.normalized * planeDistance;
+					//moveDirection = new Vector3(Input);
+				}
+			}
+			else
+			{
+				moveDirection = new Vector3(Input.GetAxis(horizontalAxis), 0, Input.GetAxis(verticalAxis));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
 			}
 		}
 
