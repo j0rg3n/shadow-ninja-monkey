36136b8badd3fe74d5a846b912431af35d029f05
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-30 23:50:20 +0000

    RPC calls on player hitting player.

--- trunk/Spikes/Assets/Scripts/PokerCollider.cs
+++ trunk/Spikes/Assets/Scripts/PokerCollider.cs
@@ -1,21 +1,66 @@
 using UnityEngine;
 using System.Collections;
 
-public class PokerCollider : MonoBehaviour {
+[RequireComponent(typeof(NetworkView))]
+public class PokerCollider : MonoBehaviour 
+{
+	public Material pokingMaterial;
+	public float flashRate = 7;
+	
+	private Material defaultMaterial;
 
-	// Use this for initialization
-	void Start () {
+	private MeshRenderer meshRenderer;
+	private float flashEndTime = 0;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Start()
+	{
+		meshRenderer = GetComponentInChildren<MeshRenderer>();
+		if (meshRenderer != null)
+		{
+			defaultMaterial = meshRenderer.material;
+		}
 	}
 	
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "Sword")
-			Debug.Log(this.name + " was Poked !");
+		if (other.tag == "Sword")
+		{
+			Debug.Log(name + " was Poked !");
+
+			if (networkView != null && networkView.isMine)
+			{
+				// We have only two players, so RPCMode.Others is in effect the other player.
+				networkView.RPC("OnPokedOpponent", RPCMode.Others);
+			}
+		}
+	}
+
+	void Update()
+	{
+		if (meshRenderer != null)
+		{
+			float time = Time.time;
+			bool flashing = time < flashEndTime;
+			if (flashing)
+			{
+				bool on = ((int)(time * flashRate) & 1) == 1;
+				meshRenderer.material = on ? pokingMaterial : defaultMaterial;	
+			}
+			else if (meshRenderer.material != defaultMaterial)
+			{	
+				meshRenderer.material = defaultMaterial;
+			}
+		}
+	}
+	
+	[RPC]
+	void OnPokedOpponent()
+	{
+		Flash();
+	}
+	
+	private void Flash()
+	{
+		flashEndTime = Time.time + 4;
 	}
 }
