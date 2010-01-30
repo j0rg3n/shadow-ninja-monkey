a574b59e58a66490b5178b798d6fc2f833b2214f
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-30 14:22:29 +0000

    Added ninja animation test.

---
+++ Spikes/Assets/Scripts/NinjaPokerAnimation.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections;
+
+public class NinjaPokerAnimation : MonoBehaviour 
+{
+	public float speed = 5.0f;
+	public Quaternion blockRotation = Quaternion.Euler(90, 0, 0);
+	public Quaternion pokeRotation = Quaternion.Euler(0, 0, 90);
+	public Quaternion standRotation = Quaternion.Euler(0, 0, 0);
+	
+	// Store separately, as Transform is handled by reference, not by value.
+	private Quaternion defaultRotation;
+	private Vector3 defaultPosition;
+
+	// Interpolation state
+	private Quaternion targetRotation;
+	private Quaternion currentRotation;
+	private float moveStartTime = -1000.0f;
+
+	public void Poke()
+	{
+		SetTargetRotation(pokeRotation);
+	}
+	
+	public void Block()
+	{
+		SetTargetRotation(blockRotation);
+	}
+	
+	public void Stand()
+	{
+		SetTargetRotation(defaultRotation);
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		defaultPosition = transform.localPosition;
+		defaultRotation = transform.localRotation;
+		targetRotation = defaultRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float t = (Time.time - moveStartTime) * speed;
+		
+		Quaternion r;
+		if (t <= 0)
+		{
+			r = currentRotation;
+		}
+		else if (t < 1)
+		{
+			r = Quaternion.Slerp(currentRotation, targetRotation, t);
+		}
+		else
+		{
+			r = targetRotation;
+		}
+		
+		transform.localRotation = r * defaultRotation;
+		transform.localPosition = r * defaultPosition;
+	}
+
+	private void SetTargetRotation(Quaternion newTargetRotation)
+	{
+		currentRotation = targetRotation;
+		moveStartTime = Time.time;
+		targetRotation = newTargetRotation;
+	}
+}
