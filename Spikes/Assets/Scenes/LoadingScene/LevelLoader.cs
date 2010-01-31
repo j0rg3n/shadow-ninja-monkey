0f626bf70b1ca2e9d5426572b130aa692c790524
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-31 03:22:15 +0000

    Black ninja now created by client.

--- Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
+++ Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
@@ -9,16 +9,27 @@ public class LevelLoader : MonoBehaviour {
 
 	public NinjaLevel LoadLevel002(bool instantiateOnNetwork)
 	{
-		NinjaLevel level= FlexiInstantiate<NinjaLevel>(level002, new Vector3(0,0,0), Quaternion.identity, instantiateOnNetwork);
-		mainCamera.transform.position = level.cameraPosition;
-		
-		NinjaBehaviour blackNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, level.blackNinjaPosition, Quaternion.identity, instantiateOnNetwork);
-		blackNinja.Initialize(NinjaBehaviour.NinjaColor.Black, instantiateOnNetwork);
-		
-		NinjaBehaviour whiteNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, level.whiteNinjaPosition, Quaternion.identity, instantiateOnNetwork);
-		whiteNinja.Initialize(NinjaBehaviour.NinjaColor.White, instantiateOnNetwork);
+		NinjaLevel level = null;
+		if (Network.peerType == NetworkPeerType.Server)
+		{
+			level = FlexiInstantiate<NinjaLevel>(level002, new Vector3(0,0,0), Quaternion.identity, instantiateOnNetwork);
+			mainCamera.transform.position = level.cameraPosition;
+			
+			NinjaBehaviour whiteNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, level.whiteNinjaPosition, Quaternion.identity, instantiateOnNetwork);
+			whiteNinja.Initialize(NinjaBehaviour.NinjaColor.White, instantiateOnNetwork);
+			
+			networkView.RPC("CreateYourBlackClientNinja", RPCMode.Others, level.blackNinjaPosition);
+		}
 		
 		return level;
+	}
+	
+	[RPC]
+	public void CreateYourBlackClientNinja(Vector3 blackNinjaPosition)
+	{
+		const bool instantiateOnNetwork = true;
+		NinjaBehaviour blackNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, blackNinjaPosition, Quaternion.identity, instantiateOnNetwork);
+		blackNinja.Initialize(NinjaBehaviour.NinjaColor.Black, instantiateOnNetwork);
 	}
 	
 	private T FlexiInstantiate<T>(Object original, Vector3 position, Quaternion rotation, bool instantiateOnNetwork) where T : Component
