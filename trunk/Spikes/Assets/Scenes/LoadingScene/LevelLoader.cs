74ab8122d9a9549f672e848b80e0445997c078cf
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-31 03:40:25 +0000

    Fixed instantiation code, delayed till player connect.

--- trunk/Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
+++ trunk/Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
@@ -6,19 +6,26 @@ public class LevelLoader : MonoBehaviour {
 	public Camera mainCamera;
 	public GameObject level002;
 	public GameObject ninjaCharacter;
+	
+	private NinjaLevel level = null;
 
 	public NinjaLevel LoadLevel002(bool instantiateOnNetwork)
 	{
-		NinjaLevel level = null;
-		if (Network.peerType == NetworkPeerType.Server)
+		if (instantiateOnNetwork)
+		{
+			if (Network.peerType == NetworkPeerType.Server)
+			{
+				level = FlexiInstantiate<NinjaLevel>(level002, new Vector3(0,0,0), Quaternion.identity, instantiateOnNetwork);
+				mainCamera.transform.position = level.cameraPosition;
+			}
+		}
+		else
 		{
 			level = FlexiInstantiate<NinjaLevel>(level002, new Vector3(0,0,0), Quaternion.identity, instantiateOnNetwork);
 			mainCamera.transform.position = level.cameraPosition;
-			
-			NinjaBehaviour whiteNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, level.whiteNinjaPosition, Quaternion.identity, instantiateOnNetwork);
-			whiteNinja.Initialize(NinjaBehaviour.NinjaColor.White, instantiateOnNetwork);
-			
-			networkView.RPC("CreateYourBlackClientNinja", RPCMode.Others, level.blackNinjaPosition);
+
+			CreateNinja(NinjaBehaviour.NinjaColor.White, level.whiteNinjaPosition, instantiateOnNetwork);
+			CreateNinja(NinjaBehaviour.NinjaColor.Black, level.blackNinjaPosition, instantiateOnNetwork);
 		}
 		
 		return level;
@@ -29,8 +36,21 @@ public class LevelLoader : MonoBehaviour {
 	{
 		Debug.Log("WE ARE CREATING A BLACK CLIENT NINJA....");
 		const bool instantiateOnNetwork = true;
-		NinjaBehaviour blackNinja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, blackNinjaPosition, Quaternion.identity, instantiateOnNetwork);
-		blackNinja.Initialize(NinjaBehaviour.NinjaColor.Black, instantiateOnNetwork);
+		CreateNinja(NinjaBehaviour.NinjaColor.Black, blackNinjaPosition, instantiateOnNetwork);
+	}
+	
+	public void OnPlayerConnected(NetworkPlayer player)
+	{
+		const bool instantiateOnNetwork = true;
+		CreateNinja(NinjaBehaviour.NinjaColor.White, level.whiteNinjaPosition, instantiateOnNetwork);
+		networkView.RPC("CreateYourBlackClientNinja", RPCMode.Others, level.blackNinjaPosition);
+	}
+
+	private void CreateNinja(NinjaBehaviour.NinjaColor color, Vector3 position, bool instantiateOnNetwork)
+	{
+		Debug.Log("Creating " + color + " local");
+		NinjaBehaviour ninja = FlexiInstantiate<NinjaBehaviour>(ninjaCharacter, position, Quaternion.identity, instantiateOnNetwork);
+		ninja.Initialize(color, instantiateOnNetwork);
 	}
 	
 	private T FlexiInstantiate<T>(Object original, Vector3 position, Quaternion rotation, bool instantiateOnNetwork) where T : Component
