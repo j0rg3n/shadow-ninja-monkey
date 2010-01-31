1065d10b290140d331786681d446483cbd74c277
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-01-31 02:23:28 +0000

    GUIRootBehaviour now instantiates level prefabs.

--- Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
+++ Spikes/Assets/Scenes/LoadingScene/LevelLoader.cs
@@ -5,23 +5,23 @@ public class LevelLoader : MonoBehaviour {
 
 	public Camera mainCamera;
 	public GameObject level002;
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	public NinjaLevel LoadLevel002()
+
+	public NinjaLevel LoadLevel002(bool instantiateOnNetwork)
 	{
-		NinjaLevel level= ((GameObject )Instantiate(level002, new Vector3(0,0,0), Quaternion.identity)).GetComponent<NinjaLevel>();
+		NinjaLevel level= ((GameObject)FlexiInstantiate(level002, new Vector3(0,0,0), Quaternion.identity, instantiateOnNetwork)).GetComponent<NinjaLevel>();
 		mainCamera.transform.position = level.cameraPosition;
 		return level;
 	}
 	
-	
+	private object FlexiInstantiate(Object original, Vector3 position, Quaternion rotation, bool instantiateOnNetwork)
+	{
+		if (instantiateOnNetwork)
+		{
+			return Network.Instantiate(original, position, rotation, 0);
+		}
+		else
+		{
+			return Instantiate(original, position, rotation);
+		}	
+	}
 }
