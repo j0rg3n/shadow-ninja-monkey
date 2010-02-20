57587042200ebd7cc8de0a48ba08b3ed9c896845
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-02-20 22:54:43 +0000

    Ah. And now it draws. Most probably in exactly the Wrong Way, but it draws.

--- trunk/jorgenhs-does-haXe/src/Test.hx
+++ trunk/jorgenhs-does-haXe/src/Test.hx
@@ -5,11 +5,30 @@
 
 //package ;
 
+import flash.events.Event;
+
 class Test 
 {
 	public static function main() 
 	{
         trace("Hello World !");		
+		
+		var mc:flash.display.MovieClip = flash.Lib.current;
+		mc.addEventListener(Event.ENTER_FRAME, onFrame, false, 0, true);
 	}
 	
+	private static function onFrame(param : Dynamic)
+	{
+		++i;
+		
+		var mc:flash.display.MovieClip = flash.Lib.current;
+        mc.graphics.beginFill(0xFF0000);
+        mc.graphics.moveTo(50,50 + i);
+        mc.graphics.lineTo(100,50 + i);
+        mc.graphics.lineTo(100,100 + i);
+        mc.graphics.lineTo(50,100 + i);
+        mc.graphics.endFill();
+	}
+	
+	static var i : Int = 0;
 }
