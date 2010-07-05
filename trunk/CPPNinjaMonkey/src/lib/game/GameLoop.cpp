0ad9923639be6835a417a71f9f80938b1b73c94a
j0rg3n <j0rg3n@3891bb16-0d8a-11df-a8f6-f3887e771010>
2010-07-05 21:28:08 +0000

    Factored PeerClient into PeerServerSession; now only difference is whether the connection is initiated here or there. Plugged in GameNetworkPacketTranslator, so it is actually responsible for handling the packets. Next step is to resume work on NAT breakthrough and implement a few entrypoints in the GameLoop.

--- trunk/CPPNinjaMonkey/src/lib/game/GameLoop.cpp
+++ trunk/CPPNinjaMonkey/src/lib/game/GameLoop.cpp
@@ -1,5 +1,7 @@
 #include "GameLoop.h"
 
+#include "diag/Trace.h"
+
 
 // ----------------------------------------------------------------------------
 
@@ -7,9 +9,25 @@
 class GameLoop::Impl
 {
 public:
-	
+	void PeerJoined(SessionID nSessionID)
+	{
+		TRACE("Peer %d joined.", nSessionID);
+	}
+
+
+	void PeerLeft(SessionID nSessionID)
+	{
+		TRACE("Peer %d left.", nSessionID);
+	}
+
+
+	void UpdatePosition(SessionID nSessionID, boost::int32_t x, boost::int32_t y)
+	{
+		TRACE("Peer %d position: (%d, %d)", nSessionID, x, y);
+	}	
+
+
 private:
-	
 };
 
 
@@ -26,3 +44,20 @@ GameLoop::~GameLoop()
 	delete m_pImpl;
 }
 
+
+void GameLoop::PeerJoined(SessionID nSessionID)
+{
+	m_pImpl->PeerJoined(nSessionID);
+}
+
+
+void GameLoop::PeerLeft(SessionID nSessionID)
+{
+	m_pImpl->PeerLeft(nSessionID);
+}
+
+
+void GameLoop::UpdatePosition(SessionID nSessionID, boost::int32_t x, boost::int32_t y)
+{
+	m_pImpl->UpdatePosition(nSessionID, x, y);
+}
